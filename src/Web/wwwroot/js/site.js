3e0a281bcae206fc18eeb29ea28fb0400232eca0
Andrew A. Poryadin <dron600@gmail.com>
2020-04-08 14:51:36 +0300

    Через LibMan добавление библиоткие Jquery DataTables

--- src/Web/wwwroot/js/site.js
+++ src/Web/wwwroot/js/site.js
@@ -2,3 +2,17 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+$(document).ready(function() {
+
+    $('#idPatientsList').DataTable({
+        columns: [
+            {
+                title:'asdadas'
+            }]
+    }
+       
+        );
+
+
+});
